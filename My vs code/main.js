cf5c65c2cf646b9a0c58f72fed6f0d516ed22dcf
Mr ar <101287880+Arashvscode@users.noreply.github.com>
2022-03-22 16:11:58 +0430

    Add files via upload

---
+++ My vs code/main.js
@@ -0,0 +1,26 @@
+function pro(){
+   var type = "link"
+   var not = 0
+   alert= type + not;
+   try {
+   
+   alert = "hello "
+      var paro = "welcom to vs code main "
+      var pae = "2021"
+   }finally{
+   
+   }
+   
+}
+
+pro()
+
+var ninja ;
+var res ;
+if (ninja >= res){
+ alert= ""
+
+}else{
+alert ="$$$"
+}
+alert="9"
