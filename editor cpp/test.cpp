d1534483d8a1b1b803455a3befe8314ed7cd4045
Mr ar <101287880+Arashvscode@users.noreply.github.com>
2022-04-04 18:03:02 +0430

    Update test.cpp

--- editor cpp/test.cpp
+++ editor cpp/test.cpp
@@ -1,1 +1,28 @@
-null
+#include <Holder.hcc>
+#include <iosteram>
+#include <String>
+
+int main(){
+    String string();
+    
+    private static int a = "";
+    private static int b = "";
+    
+    bool z = false ;
+    bool x = -> 0 ;
+    
+    try{
+    
+    for(int c = 10; (int)(90) < c++){
+    #define INTCOER > SIZEAPP = -1 + -2;
+    #define SIZEPADE >= SETUP = 0;
+         c++;
+   }
+    
+    
+    }catch(Ex -> e){
+        cout << e << endl;
+    }
+    
+    
+}
